297b80cc554958887fe0dd850338d5367443d788
antonio de gou <thestockofantonio@gmail.com>
2023-04-17 22:03:15 +0100

    configured for multipage website

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.css'
+
+import '/assets/css/style.scss'
+// import '/about.html'
+
+
---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+console.log(resolve)
+
+export default defineConfig({
+    base: "./",
+    build: {
+      outDir : "docs",
+      cssMinify: false,
+    //   polyfillModulePreload: false,
+    //   cssCodeSplit: false,
+    //   minify: false,
+      rollupOptions: {
+        input: {
+            main: resolve(__dirname, 'index.html'),
+            about: resolve(__dirname, 'nested/about.html'),
+            me: resolve(__dirname, 'nested/me.html'),
+            // nested: resolve(__dirname, 'nested/me.html'),
+          },
+        output: {
+          entryFileNames: `assets/[name].js`,
+          chunkFileNames: `assets/[name].js`,
+          assetFileNames: `assets/[name].[ext]`,
+        //   assetsInclude: '**/*.html'
+        },
+      },
+    }
+
+  })
